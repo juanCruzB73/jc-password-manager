4665c0e20ec851cf93da2863aae09f5b9d6b54d4
juanB <juancruzberrios73@gmail.com>
2025-03-09 00:07:12 -0300

    login completado

--- jcpm-backend/src/main/java/com/jcPasswordMannager/jcpm_backend/auth/AuthService.java
+++ jcpm-backend/src/main/java/com/jcPasswordMannager/jcpm_backend/auth/AuthService.java
@@ -4,6 +4,9 @@ import com.jcPasswordMannager.jcpm_backend.jwt.JwtService;
 import com.jcPasswordMannager.jcpm_backend.user.UserModel;
 import com.jcPasswordMannager.jcpm_backend.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,11 +18,16 @@ public class AuthService {
 
     private final JwtService jwtService;
 
-    //private final AuthenticationManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
 
     public AuthResponse login(LoginRequest loginRequest) {
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),loginRequest.getPassword()));
+        UserDetails user=userRepository.findByUsername(loginRequest.getUsername()).orElseThrow();
+        String token= jwtService.getToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
     }
 
     public AuthResponse register(RegisterRequest registerRequest) {
--- jcpm-backend/src/main/java/com/jcPasswordMannager/jcpm_backend/jwt/JwtService.java
+++ jcpm-backend/src/main/java/com/jcPasswordMannager/jcpm_backend/jwt/JwtService.java
@@ -1,24 +1,27 @@
 package com.jcPasswordMannager.jcpm_backend.jwt;
 
 import com.jcPasswordMannager.jcpm_backend.user.UserModel;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static String SECRET="586E3272357538782F413F4428472B4B6250655368566B597033733676397924";
-    public String getToken(UserModel user) {
+    public String getToken(UserDetails user) {
         return getToken(new HashMap<>(),user);
     }
 
-    private String getToken(Map<String, Object>extraClaims, UserModel user) {
+    private String getToken(Map<String, Object>extraClaims, UserDetails user) {
         return Jwts
                 .builder()
                 .setClaims(extraClaims)
@@ -32,5 +35,31 @@ public class JwtService {
     private Key getKey() {
         byte[] keyBytes= Decoders.BASE64.decode(SECRET);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getClaim(token,Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username=getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+    private Claims getAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    public <T> T getClaim(String token, Function<Claims,T>claimsResolver){
+        final Claims claims=getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Date getExpirationDate(String token){
+        return getClaim(token,Claims::getExpiration);
+    }
+    private boolean isTokenExpired(String token){
+        return getExpirationDate(token).before(new Date());
     }
 }
