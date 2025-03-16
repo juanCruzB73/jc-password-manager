8463137d41685d1400b64f9a57aab8a1ef696dbb
juanB <juancruzberrios73@gmail.com>
2025-03-16 15:56:19 -0300

    arreglar credential by user

--- jcpm-backend/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (username, email, password) VALUES ('elliot', 'elliot@proton.com', 'asdw')ON CONFLICT (email) DO NOTHING;;
-INSERT INTO users (username, email, password) VALUES ('giovanni_rbn', 'giovanni.fpeople@gmail.com', 'Conejo2003.')ON CONFLICT (email) DO NOTHING;;
